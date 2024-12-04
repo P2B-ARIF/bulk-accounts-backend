b68959de72d02c7ec11623a5d61c94308ea3ae4e
MD Arif <p2b.business.info@gmail.com>
2024-12-04 13:51:05 +0600

    maintenance fix

--- src/modules/userDailyStats/dailyStats.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const authMiddleware = require("../../middlewares/authMiddleware");
-const router = express.Router();
-
-// router.get("/", authMiddleware, getUserStats);
-
-module.exports = router;
